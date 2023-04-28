17967329ab4d2f9330a987f17277d4649649f400
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-04-28 11:44:53 -0700

    Created a profile page file

---
+++ lib/screens/profile_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// ------------------------ //
+// ----- Profile Page ----- //
+// ------------------------ //
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+  // Leave the appBar null, for now, I will be creating a appBar class for that! - Joushua //
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(appBar: null);
+  }
+}
