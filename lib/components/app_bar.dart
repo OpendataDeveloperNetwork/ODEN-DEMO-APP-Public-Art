c0ac72f6b72c9b76ab61b1132dec279431cec7a0
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-04-28 14:22:55 -0700

    added an appBar()

---
+++ lib/components/app_bar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../theme/style.dart';
+
+AppBar appBar() {
+  return AppBar(
+    backgroundColor: appBarTheme().backgroundColor,
+    leading: Padding(
+      padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+      child: Image.asset(
+        ('assets/images/logo.png'),
+      ),
+    ),
+    leadingWidth: 110,
+    toolbarHeight: 60,
+    actions: [
+      Transform.scale(
+        scale: 1.50,
+        child: profileButton(),
+      )
+    ],
+  );
+}
+
+IconButton profileButton() {
+  return IconButton(
+    splashColor: appBarTheme().actionsIconTheme?.color,
+    icon: Icon(
+      Icons.account_circle_rounded,
+      color: appBarTheme().iconTheme?.color,
+    ),
+    onPressed: () => {},
+  );
+}
