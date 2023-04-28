76fd62bee025429431db6f0eafb8de22343c82ad
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-04-28 11:46:10 -0700

    Created a maps page file

---
+++ lib/screens/map_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// ------------------------------------- //
+// ----- Maps Page - Main Feature ------ //
+// ------------------------------------- //
+
+class MapsPage extends StatelessWidget {
+  const MapsPage({super.key});
+  // Leave the appBar null, for now, I will be creating a appBar class for that! - Joushua //
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(appBar: null);
+  }
+}
