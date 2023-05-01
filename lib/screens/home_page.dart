7eb07153b9e48ecfd2c4e16f510866b6e976deb4
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-05-01 08:37:48 -0700

    Finished designing the layout and creating the home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../components/app_bar.dart';
+import '../models/category.dart';
+import '../models/location.dart';
 
 // ------------------------ //
 // ----- Landing Page ----- //
@@ -10,6 +12,94 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: appBar());
+    return Scaffold(appBar: appBar(), body: const HomePageBody());
   }
+}
+
+class HomePageBody extends StatefulWidget {
+  const HomePageBody({super.key});
+
+  @override
+  State<HomePageBody> createState() => _HomePageBodyState();
+}
+
+class _HomePageBodyState extends State<HomePageBody> {
+  late Size size;
+  late double height;
+  final Category _category = Category("Public Art", "public-art");
+  final Location _location = Location("Current Location", 0, 0);
+
+  @override
+  Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    height = size.height;
+
+    return Column(children: [
+      const Expanded(
+          child: Center(
+              child: Text(
+        "Let's Get Started",
+        style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+      ))),
+      Expanded(
+        flex: 3,
+        child: Column(
+          children: [
+            _buildButton(_category, "Category"),
+            _buildButton(_location, "Location"),
+            SizedBox(height: height * 0.10),
+            _buildRetrieveButton()
+          ],
+        ),
+      ),
+    ]);
+  }
+}
+
+Container _buildButton(value, String title) {
+  return Container(
+      padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.left,
+          ),
+          Container(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+              child: SizedBox(
+                  height: 45,
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    onPressed: () => {},
+                    style: OutlinedButton.styleFrom(
+                        textStyle: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w500),
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        side: const BorderSide(
+                            color: Color(0xFF77BF4B), width: 2)),
+                    child: Text(value.name),
+                  )))
+        ],
+      ));
+}
+
+Container _buildRetrieveButton() {
+  return Container(
+      padding: const EdgeInsets.fromLTRB(50, 10, 50, 10),
+      width: double.infinity,
+      height: 65,
+      child: ElevatedButton(
+          onPressed: () => {},
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              backgroundColor: const Color(0xFF2D3848),
+              textStyle:
+                  const TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+          child: const Text("Retrieve")));
 }
