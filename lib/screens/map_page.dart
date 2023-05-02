47726065cde402d7927c235946ae065511e085d7
Pahul-Sidhu <97401471+Pahul-Sidhu@users.noreply.github.com>
2023-05-02 09:55:28 -0700

    added firebase

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:ffi';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geocoding/geocoding.dart';
@@ -11,9 +12,23 @@ import 'package:firebase_core/firebase_core.dart';
 // ----- Maps Page - Main Feature ------ //
 // ------------------------------------- //
 
-class Data{
-  void fetchData(){
+class Data {
+  Data() {
+    fetchData();
+  }
 
+  Future<void> fetchData() async {
+    var fire_base = await Firebase.initializeApp();
+    var db = FirebaseFirestore.instance;
+    db.collection("PublicArts").get().then(
+          (querySnapshot) {
+        print("Successfully completed");
+        for (var docSnapshot in querySnapshot.docs) {
+          print('$docSnapshot');
+        }
+      },
+      onError: (e) => print("Error completing: $e"),
+    );
   }
 }
 
@@ -25,7 +40,10 @@ class MapsPage extends StatefulWidget {
 }
 
 class _MapsPageState extends State<MapsPage> {
-  CameraPosition _kGooglePlex = const CameraPosition(target: LatLng(0, 0), zoom: 14.4746);
+  Data data = Data();
+
+  CameraPosition _kGooglePlex = const CameraPosition(
+      target: LatLng(0, 0), zoom: 14.4746);
 
   @override
   void initState() {
@@ -37,8 +55,8 @@ class _MapsPageState extends State<MapsPage> {
     try {
       Position pos = await getCurrentLocation();
       setState(() {
-        _kGooglePlex = CameraPosition(target: LatLng(pos.latitude, pos.longitude), zoom: 14.4746);
-        print("latlong  ${pos.latitude}  ${pos.longitude}");
+        _kGooglePlex = CameraPosition(
+            target: LatLng(pos.latitude, pos.longitude), zoom: 14.4746);
       });
     } catch (e) {
       debugPrint("Error");
@@ -86,7 +104,8 @@ class _MapsPageState extends State<MapsPage> {
             //   child: null
             // ),
             Expanded(
-              child: _kGooglePlex.target.longitude == 0 ? const Center(child: CircularProgressIndicator()) : GoogleMap(
+              child: _kGooglePlex.target.longitude == 0 ? const Center(
+                  child: CircularProgressIndicator()) : GoogleMap(
                 mapType: MapType.normal,
                 initialCameraPosition: _kGooglePlex,
                 onMapCreated: (GoogleMapController controller) {
