c808b9e10cb75a3224be72a8bc5071c0b0e4f9c8
pcammayo <pcammayo03@gmail.com>
2023-05-02 14:25:00 -0700

    Implemented firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import './screens/home_page.dart';
 import './screens/map_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   // For simplicity, for now, just add your class on the home!
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'ODEN Demo',
       home: MapsPage(),
     );
