ee8b9cb11009ef8a296e49a99de2119a565a270a
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-05-02 13:45:09 -0700

    Added a another app bar

---
+++ lib/components/back_button_app_bar.dart
@@ -0,0 +1,5 @@
+///
+/// This app bar is used for the profile page or other
+/// pages that needs a back button on the app bar. Please
+/// choose this iff **you need a back button on the app bar**.
+///
