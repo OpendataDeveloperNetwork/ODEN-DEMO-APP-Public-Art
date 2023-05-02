741051a48ea35739448b6e138e51234aafa4f5ca
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-05-02 15:51:44 -0700

    Removed unused imports

--- lib/screens/components/favourites_list_view.dart
+++ lib/screens/components/favourites_list_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 ///
 /// This is a list view widget for all the favourites.
