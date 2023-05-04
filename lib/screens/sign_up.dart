ff9b0d0fe569cae117f2f41d353136b924ae21e2
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-05-04 10:10:49 -0700

    Created a sign Up screen

---
+++ lib/screens/sign_up.dart
@@ -0,0 +1,15 @@
+// ------------------------ //
+// ----- Sign Up Page ----- //
+// ------------------------ //
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
