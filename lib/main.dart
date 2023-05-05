9c25dc59c3350c2e3544d5f390fa4d472089b0e8
Prabh <prabhjeetsokhey@gmail.com>
2023-05-05 11:43:12 -0700

    commnets for running dummy manifest in main, commented out to not keep running on firestore.

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,10 @@ Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  processPublicArtData();
+  // Following function runs the dummy minefest.dart
+  // will upload all entries to the firestore.
+  // commented out not run run everytime.
+  //processPublicArtData();
 
   runApp(const MyApp());
 }
