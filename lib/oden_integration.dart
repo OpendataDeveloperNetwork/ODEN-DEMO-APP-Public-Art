1f7a1b167cb783e927242ccf786a317648257762
Prabh <prabhjeetsokhey@gmail.com>
2023-05-08 13:02:28 -0700

    used for ODEN intigration functions.

---
+++ lib/oden_integration.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import './dummy_manifest.dart';
+
+
+Future<void> uploadStandardizedDataToFirestore() async {
+  // Get standardized data
+  List<Map<String, dynamic>> standardizedData = await processPublicArtData();
+
+  // Firestore reference
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  // Get the reference to the 'Items' collection
+  // use 'Items' for in use db and 'Manifest_test' for testing menifest.
+  CollectionReference itemsCollection = firestore.collection('Categories')
+      .doc('Public_Art')
+  //.collection('Items');
+      .collection('Manifest_test');
+
+  // Upload each element of standardizedData as its own document
+  for (var item in standardizedData) {
+    // Check for duplicates based on country, region, and city labels
+    QuerySnapshot querySnapshot = await itemsCollection
+        .where('labels.countryCode', isEqualTo: item['labels']['country'])
+        .where('labels.regionCode', isEqualTo: item['labels']['region'])
+        .where('labels.cityCode', isEqualTo: item['labels']['city'])
+        .where('name', isEqualTo: item['name'])
+        .get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      // If a duplicate is found, update the existing document
+      DocumentReference docRef = querySnapshot.docs.first.reference;
+      await docRef.update({
+        'name': item['name'],
+        'coordinates': item['coordinates']
+      });
+    } else {
+      // If no duplicate is found, create a new document knowing public art schema from ODEN (simple)
+      DocumentReference docRef = itemsCollection.doc();
+      await docRef.set({
+        'labels': {
+          'countryCode': item['labels']['country'],
+          'regionCode': item['labels']['region'],
+          'cityCode': item['labels']['city'],
+        },
+        'name': item['name'],
+        'coordinates': item['coordinates']
+      });
+    }
+  }
+  print('Data uploaded to Firestore successfully.');
+}
