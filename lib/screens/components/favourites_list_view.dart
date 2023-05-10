a1a990b7e97e7eee8b4cc599e0b58aa4f9204719
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-05-10 10:48:06 -0700

    Created a card and be able to show all favourites

--- lib/screens/components/favourites_list_view.dart
+++ lib/screens/components/favourites_list_view.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
+import '../../models/profile_public_art.dart';
 
 ///
 /// This is a list view widget for all the favourites.
 ///
 
 class FavouritesListView extends StatefulWidget {
-  const FavouritesListView({super.key});
+  final List<ProfilePublicArt> _favourites;
+  const FavouritesListView(this._favourites, {super.key});
 
   @override
   State<FavouritesListView> createState() => _FavouritesListViewState();
 }
 
 class _FavouritesListViewState extends State<FavouritesListView> {
+  void unFavourite(callback, publicArtId) {
+    setState(() {
+      callback();
+    });
+    widget._favourites.removeWhere((element) => element.id == publicArtId);
+  }
+
+  Dismissible cardBuilder(ProfilePublicArt favorite) {
+    return Dismissible(
+      key: Key(favorite.id),
+      child: Row(children: [
+        Expanded(
+            child: ListTile(
+                title: Text(favorite.name), subtitle: Text(favorite.date))),
+        IconButton(
+          onPressed: () => unFavourite(favorite.toggleFavourite, favorite.id),
+          icon: Icon(
+            favorite.isFavourited ? Icons.star : Icons.star_outline,
+            color: Colors.yellow,
+          ),
+          iconSize: 45,
+        )
+      ]),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 10,
+        itemCount: widget._favourites.length,
         itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: Row(children: const [
-              Expanded(child: ListTile(title: Text("Favourites")))
-            ]),
-          );
+          var favorite = widget._favourites[index];
+          return cardBuilder(favorite);
         });
   }
 }
