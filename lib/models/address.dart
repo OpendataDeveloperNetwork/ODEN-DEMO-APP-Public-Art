78185b288f4aca23d9ff6577ca026a76f2b7eaa2
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-05-12 12:41:59 -0700

    Created a Address object

---
+++ lib/models/address.dart
@@ -0,0 +1,15 @@
+class Address {
+  String country;
+  String region;
+  String city;
+  String street;
+  String code;
+
+  Address({
+    required this.country,
+    required this.region,
+    required this.city,
+    required this.street,
+    required this.code,
+  });
+}
